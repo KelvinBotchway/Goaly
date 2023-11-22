9eb880327037b26ef08899e6262decd2175b649d
root <root@Kali2.Kali2>
2023-11-22 16:22:30 +0000

    <files working

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-console.log("Hello world")
+function togglePassword(passwordId, eyeIconId) {
+  let passwordField = document.getElementById(passwordId);
+  let eyeIcon = document.getElementById(eyeIconId);
 
-//Submit Button
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    eyeIcon.src = "eye-open.png";
+  } else {
+    passwordField.type = "password";
+    eyeIcon.src = "eye-close.png";
+  }
+}
+
 function Verify() {
-    var userRef = "Bob";
-    var passRef = "Love";
-    
-    var user =  document.getElementById("username").value;
-    var pass = document.getElementById("password").value;
-    if (user == userRef && pass == passRef) {
-    alert("Connection Suceeded")
-    window.location.href = "newpass.html"
-    
-    } else {
-    alert("It's seem you made a mistake...")
-    }
-  };
+  var userRef = "Bob";
+  var passRef = "Love";
+
+  var user = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+  
+
+  if (user === userRef && pass === passRef) {
+    alert("Connection Succeeded");
+    window.location.href = "newpass.html";
+  } else {
+    alert("It seems you made a mistake...");
+  }
+}
